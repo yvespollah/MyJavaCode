297285363a194e569a64db80040e2f167a9a4c26
pollah yves <yvesfranky961@gmail.com>
2023-11-04 11:28:27 +0100

    Exercice2 application de notion d'heritage

--- FicheTd3INF331JavaOO/Execice1/exo1.java
+++ FicheTd3INF331JavaOO/Execice1/exo1.java
@@ -1,28 +1,15 @@
 package Execice1;
 
 public class exo1{
-   public static void main(String[] args) {
-      
-      Point point1 =  new Point();
-      point1.Afficher();
-      point1.deplacer(1, 2);
-      point1.Afficher();
 
-      Point point2 =  new Point();
-      point2.Afficher();
-      point2.initialiser(1,1);
-      point2.Afficher();
+   public  static class Point{
+    public double abscisse;
+    public double ordonnee;
 
-      point2.deplacer(1, 2);
-      point2.Afficher();
-
-      
-   }
-    
-}
-     class Point{
-    private double abscisse;
-    private double ordonnee;
+      public Point(double abscisse , double ordonnee){
+         this.abscisse  = abscisse;
+         this.ordonnee = ordonnee;
+      }
 
 
      public void initialiser(double x , double y){
@@ -39,5 +26,43 @@ public class exo1{
      public void Afficher(){
         System.out.println("je suis un point de coordonnees "+abscisse +" et " + ordonnee);
      }
+
+   }
+
+public static class Pointcol extends Point{
+   private byte couleur;
     
+      public Pointcol(double abscisse , double ordonnee , byte couleur){
+         super(abscisse, ordonnee);
+         this.couleur = couleur;
+      }
+
+    public void colorer(byte color){
+      this.couleur = color;
+    }
+
+    public void afficheCol(){
+      System.out.println("je suis un point de coordonnees "+abscisse +" et " + ordonnee);
+      System.out.println("jai une couleur "+couleur);
+    }
+}
+
+public static void main(String[] args) {
+
+   Pointcol point1 =  new Pointcol(2,4,(byte)1);
+   
+   point1.afficheCol();
+   point1.deplacer(1, 2);
+   point1.afficheCol();
+
+   Point point2 =  new Point(2,4);
+   point2.Afficher();
+   point2.initialiser(1,1);
+   point2.Afficher();
+
+   point2.deplacer(1, 2);
+   point2.Afficher();   
+
+
+}
 }
